f487eb08cbbefcee08feaddbf34bbb170e058c23
faraz.usman <farazusman5@gmail.com>
2025-04-13 15:08:51 +0500

    Food expense report

--- APIs/reports.js
+++ APIs/reports.js
@@ -49,6 +49,44 @@ router.get("/EXPENSE", (req, res) => {
     });
 });
 
+router.get("/INVENTORY", (req, res) => {
+    db.all("SELECT * FROM product", [], async (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            return res.status(500).json({ error: "Internal server error" });
+        }
+
+        try {
+            const rowsWithLedger = await Promise.all(
+                rows.map(row => {
+                    return new Promise((resolve, reject) => {
+                        db.all("SELECT * FROM inventoryLedger WHERE product_id = ?", [row.id], (err, ledgerRows) => {
+                            if (err) {
+                                return reject(err);
+                            }
+                            // remove user object from ledgerrows
+                            const formattedRows = ledgerRows.map(ledgerRow => {
+                                const { user, ...rest } = ledgerRow;
+                                return rest;
+                            });
+                            row.ledger = formattedRows;
+                            resolve(row);
+                        });
+                    });
+                })
+            );
+
+            // filter out the objects that don't have ledger
+            const response = rowsWithLedger.filter(row => row.ledger.length > 0);
+            res.json({ data: response, totalData: response.length });
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).json({ error: "Error fetching ledger data" });
+        }
+    });
+});
+
 
 
 module.exports = router;
